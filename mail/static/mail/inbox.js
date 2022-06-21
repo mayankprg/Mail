2cb0a152c60d19a2ce23fa198113aa7bebb34cd0
Mayank Rawat <mayankrawatprg@gmail.com>
2022-06-21 16:42:56 +0530

    added fetch request to email compose

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -8,7 +8,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
+
+  document.querySelector('#compose-form').addEventListener('submit', event => {
+    event.preventDefault();
+
+    var recipients = document.querySelector('#compose-recipients').value;
+    var subject = document.querySelector('#compose-subject').value;
+    var body = document.querySelector('#compose-body').value;
+
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body,
+      })
+    })
+    .then(response => response.JSON())
+    .then(result => {
+      console.log(result)
+    });
+    
+  });
+
 });
+
 
 function compose_email() {
 
@@ -31,3 +55,5 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
+
+
