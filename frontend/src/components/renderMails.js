10d37c52ed3c8bdbd3b48352c3d4da0c7a92273f
mayankprg <mayankrawatprg@gmail.com>
2022-10-06 00:47:32 +0530

    fixed email divs

--- frontend/src/components/renderMails.js
+++ frontend/src/components/renderMails.js
@@ -4,35 +4,41 @@ import { useNavigate } from "react-router-dom";
 
 const RenderMails = ({data}) => {
 
+    const navigate = useNavigate();
 
-	
-	let navigate = useNavigate();
     return (
+
 		<div className={renderCSS["email-data"]}>
-				{data.map((mail) =>
-						<div className={mail.read ? renderCSS["read"]: renderCSS["emails-div"] } key={mail.id} onClick={() =>{navigate(`/email/${mail.id}`);}}>
-							<table className={renderCSS["content-tb"]}>
-								<tbody>
-								<tr>
-									<td className={`${renderCSS["sender"]}`}>
-										{mail.sender}
-									</td>
-									<td className={`${renderCSS["truncate"]}`}>
-										<span className={` ${renderCSS["subject"]}`} >{mail.subject? mail.subject: "(no subject)"}</span>
-										<span className={` ${renderCSS["body"]}`}>-{mail.body}</span>
-									</td>
-									<td className={`${renderCSS["date"]}`}>
-										{
-											new Date().getFullYear() === new Date().getFullYear(mail.timestamp) 
-												? new Date(mail.timestamp).toLocaleString('en-us',{month:'short', day: 'numeric'}) 
-												: new Date(mail.timestamp).toLocaleString('en-us',{day:'numeric', month:'numeric', year: 'numeric'})
-										}
-									</td>
-								</tr>
-								</tbody>
-							</table>
-						</div>
-				)}
+            {data.map((mail) =>
+                <div className={mail.read ? renderCSS["read"]: renderCSS["uread"]}
+                    key={mail.id} 
+                    onClick={() =>{navigate(`/email/${mail.id}`);}}>
+
+                        <div className={renderCSS.container}>
+
+                            <div className={renderCSS.item1}>
+                                <span className={renderCSS.sender}>{mail.sender}</span>
+                            </div>
+
+                            <div className={renderCSS.item2}>
+                                <span className={renderCSS.subject}>
+                                    {mail.subject? mail.subject: "(no subject)"}
+                                </span>
+                                <span className={renderCSS.body}>
+                                    - {mail.body}
+                                </span>
+                            </div>
+
+                            <div className={renderCSS.item3}>
+                                <span className={renderCSS.date}>
+                                    {new Date().getFullYear() === new Date().getFullYear(mail.timestamp) 
+                                        ? new Date(mail.timestamp).toLocaleString('en-us',{month:'short', day: 'numeric'}) 
+                                        : new Date(mail.timestamp).toLocaleString('en-us',{day:'numeric', month:'numeric', year: 'numeric'})}
+                                </span>
+                            </div>      
+                        </div>
+                </div>
+            )}
 		</div>
 	)
 		
