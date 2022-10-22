1c046817f2483f0bd9719c0658735d8a09e33579
mayankprg <mayankrawatprg@gmail.com>
2022-10-22 22:43:37 +0530

    add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM  python:3
+
+COPY . /usr/src/app
+WORKDIR /usr/src/app
+
+RUN pip install -r requirements.txt
+
+EXPOSE 8000
--- project3/settings.py
+++ project3/settings.py
@@ -139,12 +139,24 @@ WSGI_APPLICATION = 'project3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'my_service',
+            'passfile': '.my_pgpass',
+        },
     }
 }
+
+
 
 AUTH_USER_MODEL = 'mail.User'
 
