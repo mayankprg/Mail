632af0793dc87af47d19f826cbc43335c1de3441
mayankprg <mayankrawatprg@gmail.com>
2022-10-25 06:55:05 +0530

    test 1

--- project3/settings.py
+++ project3/settings.py
@@ -199,6 +199,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
 
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = BASE_DIR
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
