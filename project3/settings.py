91b50263f903f9532f489e90d11ce34a2b54f38a
mayankprg <mayankrawatprg@gmail.com>
2022-10-25 07:56:05 +0530

    test 1

--- project3/settings.py
+++ project3/settings.py
@@ -151,7 +151,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': 5432,
 
     }
