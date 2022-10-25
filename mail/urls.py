1998a75a82231936ce3359663f00f648c90ade29
mayankprg <mayankrawatprg@gmail.com>
2022-10-25 08:17:11 +0530

    test 1

--- mail/urls.py
+++ mail/urls.py
@@ -8,14 +8,15 @@ from . import views
 
 urlpatterns = [
 
-    path("", views.index, name="index"),
+    path("/", views.index, name="index"),
 
     # API Routes
     path("api/emails", views.compose, name="compose"),
     path("api/emails/<int:email_id>", views.email, name="email"),
     path("api/emails/<str:mailbox>", views.mailbox, name="mailbox"),
 
-    path('api/token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', views.MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/signup/', views.signup, name='signup'),
 
