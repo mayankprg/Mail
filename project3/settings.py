24d0c6c8e0c3d69d7bf55b0c9649049e65c001fe
mayankprg <mayankrawatprg@gmail.com>
2022-10-25 08:22:49 +0530

    test 1

--- project3/settings.py
+++ project3/settings.py
@@ -196,9 +196,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
 
 
 STATIC_URL = '/static/'
