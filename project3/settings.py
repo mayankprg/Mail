ace8be74b44897371d6e95c1ff550643e3c42533
mayankprg <mayankrawatprg@gmail.com>
2022-10-25 09:59:57 +0530

    test 1

--- project3/settings.py
+++ project3/settings.py
@@ -196,14 +196,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
 
 
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_ROOT = BASE_DIR
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
